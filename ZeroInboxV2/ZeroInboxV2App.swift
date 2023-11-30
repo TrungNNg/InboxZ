5b76705c0e758e5ee19dabc4c815f0c42f9f4660
Trung Nguyen <ngtrung0916@gmail.com>
2023-11-29 19:41:38 -0500

    Initial Commit

---
+++ ZeroInboxV2/ZeroInboxV2App.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroInboxV2App.swift
+//  ZeroInboxV2
+//
+//  Created by Trung Nguyen on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ZeroInboxV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
